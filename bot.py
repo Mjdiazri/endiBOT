ee637d9e3691cdac1c621088b495a315d1bb39e8
Mjdiazri <citruss2012@hotmail.es>
2026-05-30 23:06:36 -0500

    Se cambiar variables globales por return, se ajusta uso de handlers, se implementan iconos. Programa funcionando ok

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 
 
-#Read .env Telegram token
+#Read
 load_dotenv()
 TOKEN= os.getenv('BOT_TOKEN')
 bot = telebot.TeleBot(TOKEN)
@@ -13,75 +13,66 @@ bot = telebot.TeleBot(TOKEN)
 #Load json file
 with open('words.json', 'r', encoding='utf-8') as file:
     data=json.load(file)
-    print('loading json file.....')
+    print('loading json file.....👾🤖🔎🔍📌🔹🔸▫️')
 
 #Start chatbot
 @bot.message_handler(commands=['start'])
 def start(message):
     name = message.chat.first_name  
-    bot.send_message(message.chat.id, f"Hey {name}, welcome to EndiBOT!!! I can look up word meanings.")
-    msg=bot.send_message(message.chat.id, "Type a word for help you.... ")   
+    bot.send_message(message.chat.id, f"Hey {name}, welcome to EndiBOT👾!!! I can look up word meanings.")
+    msg=bot.send_message(message.chat.id, "📚Type a word and I'll help you find its meaning...")   
     bot.register_next_step_handler(msg,send_response) 
 
 #Chatbot Interaction
 @bot.message_handler(content_types=['text'])
 def send_response(message):
         print('comming message....')
-        global word
         word = message.text
-        find_word()
-        print(f"isFound: {isFound}")
-        if isFound == True:
-            print("Variable True")
-            bot.send_message(message.chat.id, f"{upperWord} \n Part of speech: {speech}\n Meaning: {meaning}\n Example: {example}")
-            msg = bot.send_message(message.chat.id, "Would you like to look up for another word?  /sure   ||      /nop")      
+        founded = find_word(word)
+
+        if founded["isFound"] == True:
+            bot.send_message(message.chat.id, f"{founded['upperWord']} \n 🔸Part of speech: {founded['speech']}\n 🔸Meaning: {founded['meaning']}\n 🔸Example: {founded['example']}")
+            msg = bot.send_message(message.chat.id, "Would you like to look up for another word?🔍  /sure   ||      /nop")      
             bot.register_next_step_handler(msg,loop_word)  
         else:
-            print(f"isFound is False")
-            bot.send_message(message.chat.id, "You got me! I dont found the word")
-            msg = bot.send_message(message.chat.id, "Would you like to look up for another word?  /sure   ||      /nop")      
+            bot.send_message(message.chat.id, "Sorry, I couldn't find that word.")
+            msg = bot.send_message(message.chat.id, "Would you like to look up another word?🔍  /sure   ||      /nop")      
             bot.register_next_step_handler(msg,loop_word)
+            
 
 #Loop
-@bot.message_handler(content_types=['text'])
 def loop_word(message):
-     print(f"loop_word word: {message.text}")
-     if message.text == "/sure":
+     decision = message.text.lower()
+     if decision == "/sure":
           print("The word is /sure")
-          msg=bot.send_message(message.chat.id, "Please type the word")
-          print(f"The word typed is {msg.text}")
+          msg=bot.send_message(message.chat.id, "Please type the word 📖")
           bot.register_next_step_handler(msg,send_response)           
-     elif message.text == "/nop":
+     elif decision == "/nop":
           print("The word is /nop") 
-          bot.send_message(message.chat.id, "See you later, aligater")
+          bot.send_message(message.chat.id, "👾See you later, aligater")
      else:
           print("Unfounded word")
-          bot.send_message(message.chat.id, "I can't understand you, See you soon")
+          bot.send_message(message.chat.id, "👾I can't understand you, See you soon")
 
 # look up the word    
-def find_word():
+def find_word(word):
     low_Word = word.lower()
     print(f"Searching for {low_Word}.....")
-
-    #Validate if the word is found 
     if data.get(low_Word):
         #Validation
         print("I got the word....")
-        
-        global isFound
-        global speech 
-        global meaning
-        global example
-        global upperWord
-        
-        isFound = True
-        speech = data[low_Word]["part_of_speech"]
-        meaning = data[low_Word]["meaning"]
-        example = data[low_Word]["example"]
-        upperWord = word.upper()
+        return{
+            "isFound": True,
+            "speech": data[low_Word]["part_of_speech"],
+            "meaning": data[low_Word]["meaning"],
+            "example": data[low_Word]["example"],
+            "upperWord": word.upper()             
+        }       
     else:
         print("You got me! I dont found the word")
-        isFound = False
+        return{
+             "isFound": False
+        }
    
 
 #Check request and status
